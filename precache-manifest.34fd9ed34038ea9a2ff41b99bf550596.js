8de8cd73a2c01779330bdee6d2c2b2e014fa6c63
Aleksey Sokolov <evilsokol@gmail.com>
2019-07-09 16:52:26 +0300

    Updates

---
+++ precache-manifest.34fd9ed34038ea9a2ff41b99bf550596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a474839a51d726e2b49bfe8c3697c76f",
+    "url": "/fun-box-level2/index.html"
+  },
+  {
+    "revision": "02e0e6b13b1424d7d5ce",
+    "url": "/fun-box-level2/static/css/main.33608833.chunk.css"
+  },
+  {
+    "revision": "7da6d2466501de0c49be",
+    "url": "/fun-box-level2/static/js/2.4a777b0d.chunk.js"
+  },
+  {
+    "revision": "02e0e6b13b1424d7d5ce",
+    "url": "/fun-box-level2/static/js/main.c4ade0b0.chunk.js"
+  },
+  {
+    "revision": "b03b858117eea02491dc",
+    "url": "/fun-box-level2/static/js/runtime~main.7833b8ca.js"
+  },
+  {
+    "revision": "2f00555cf560408566dbe91479cbf268",
+    "url": "/fun-box-level2/static/media/Pattern.2f00555c.png"
+  },
+  {
+    "revision": "843a22b66c3047efdbad2e0c32385606",
+    "url": "/fun-box-level2/static/media/PhotoCat.843a22b6.png"
+  }
+]);

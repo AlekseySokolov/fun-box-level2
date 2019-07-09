6429bbce9a17e6984722a249a59151611fd49138
Aleksey Sokolov <evilsokol@gmail.com>
2019-07-09 20:16:23 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./App.css";
 import CardsWrapper from "./components/CardsContainer/CardsWrapper";
 import {Header} from "./components/Header/Header";
-
+import {mainWrapStyle, mainPageStyle} from "./styles/styles";
 
 const App = () => {
   return (
-    <div className="main-page">
+      <section className="main-wrap" style={mainWrapStyle}>
+       <div className="main-page" style={mainPageStyle}>
         <Header/>
         <CardsWrapper/>
-    </div>
+       </div>
+      </section>
   );
 }
 
---
+++ src/styles/styles.js
@@ -0,0 +1,8 @@
+import pattern from "../img/Pattern.png"
+
+export const mainWrapStyle = {
+    backgroundImage : "url("+pattern+")"
+};
+export const mainPageStyle = {
+    background: "linear-gradient(to bottom, rgba(0,0,0,0.5) 0%,rgba(0,0,0,0.5) 1%, rgba(0,0,0,0) 50%,rgba(0,0,0,0.5) 100%)"
+};
